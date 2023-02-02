d0f3e6a8afcbefc085f5694d165dfbef0330d2e0
elchincode <elchinai@code.edu.az>
2023-02-02 17:37:42 +0400

    Static

---
+++ Extension/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Extension;
+using static Extension.User;
+
+namespace Extension
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       
+            
+            User user = new User("Elchin", "Ismayilov", "2003JUL", RegistrationMonth.july);
+
+            Console.WriteLine(user.GetDeatails());
+
+            Console.WriteLine();
+
+
+
+
+
+            User user1 = new User("Robert ", "Downey", "1965APR", RegistrationMonth.aprel);
+
+            Console.WriteLine(user1.GetDeatails());
+
+            Console.WriteLine();
+
+
+
+
+
+            User user2 = new User("Jonny", "Deep", "1963JUN", RegistrationMonth.june);
+
+            Console.WriteLine(user2.GetDeatails());
+
+            Console.WriteLine();
+
+
+
+
+
+
+            User user3 = new User("Ryan", "Reynolds", "1976OCT", RegistrationMonth.october);
+
+            Console.WriteLine(user3.GetDeatails());
+            Console.WriteLine();
+
+
+
+
+
+            User user4 = new User("Scarlett", "Johansson", "1984NO", RegistrationMonth.november);
+
+            Console.WriteLine(user4.GetDeatails());
+
+
+
+        }
+    }
+
+}
---
+++ Extension/User.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Extension
+{
+    public class User
+    {
+
+        private string pin;
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public enum RegistrationMonth 
+        {
+            january = 1,
+            february,
+            marth,
+            aprel,
+            may,
+            june,
+            july,
+            august,
+            september,
+            october,
+            november,
+            december
+
+        }
+        public RegistrationMonth registrationMonth { get; set; }
+
+
+
+
+
+        public string PIN
+
+        {
+            get { return pin; }
+
+
+            set
+            {
+                if(value.Set() == true)
+                {
+                    pin = value; 
+                }
+                else
+                {
+                    pin = "Something went wrong (((" ;
+                }
+            }
+
+
+
+        }
+
+
+
+
+        //public string PIN ////// Mellim ama mence buda bir usuldur  bunun methodu LINE:109-125
+
+        //{
+        //    get { return pin.Set(); }
+
+
+        //    set
+        //    {
+        //        pin = value;
+        //    } 
+
+
+
+        //}
+
+
+
+
+  
+
+
+
+        public User(string Name , string Surname  , string PIN , RegistrationMonth registrationMonth  )
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.registrationMonth= registrationMonth;
+            this.PIN = PIN;
+            
+
+        }
+       
+        public string GetDeatails()
+        {
+            return $"Student Name : {Name} \nStudent Surname : {Surname} \nStudent Registration Month : {registrationMonth} \nStudent PIN : {PIN}";
+        }
+
+
+
+
+    }
+
+    //public static class user   ///
+    //{
+    //    public static string Set(this string PIN)
+    //    {
+    //        if (PIN.Length == 7 && PIN == PIN.ToUpper()) 
+    //        {
+    //            return PIN;
+    //        }
+    //        else
+    //        {
+    //            return "error" ;
+    //        }
+
+
+    //    }
+
+    //}
+    public static class user
+    {
+        public static  bool Set(this string PIN)
+        {
+            if (PIN.Length == 7 && PIN == PIN.ToUpper())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+
+}
